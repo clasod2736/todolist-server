9224a532df8bd83337503156118ca6dbb24bf03e
clasod2736 <109887795+clasod2736@users.noreply.github.com>
2023-04-18 16:24:28 +1000

    good.

---
+++ front-end/src/components/Todo/Todo.jsx
@@ -0,0 +1,58 @@
+import React, { useState } from 'react'
+import './Todo.css'
+import axios from 'axios'
+
+export default function Todo({ id, todo, onUpdate, onDelete, minusCount }) {
+    const [line, setLine] = useState([])
+
+    const handleChecked = (e) => {
+        onUpdate({...todo, status: e.target.checked ? 'completed' : 'active'});
+      }
+    const lineText = (e) => {
+        if (todo.status === 'completed') {
+          setLine(<label style={{ textDecoration: 'line-through'}}>{todo.job}</label>);
+          e.preventDefault();
+        } else if (todo.status === 'active') {
+            e.preventDefault();
+            setLine(todo.job);
+        }};
+    const newText = () => {
+        if (todo.status === 'completed') {
+          return line;
+        } else {
+          return todo.job
+        }}
+      const handleDelete = async (e) => {
+        onDelete(todo);
+
+        //개별삭제 API 요청
+        await axios.delete('http://localhost:8080/deleteOne', {
+          data: {
+          "id": id
+        }})
+        .then(function (response) {
+          console.log(response);
+        })
+        .catch(function (error) {
+          console.log(error);
+        });
+        e.preventDefault();
+        minusCount();
+      }
+
+    return (
+        <li key={id}>
+            <input 
+            type='checkbox'
+            id='checkbox'
+            className='checkbox'
+            onChange={handleChecked}
+            onInputCapture={lineText}
+            />
+        <label htmlFor="checkbox">{newText()}</label>
+        <button className='deleteBtn'
+        onClick={handleDelete}
+        >X</button>
+        </li>
+  )
+}
